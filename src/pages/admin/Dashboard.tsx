63bf0bbf77c6d6d5419aadbd53bb00968994732a
nguyenxuantra <nguyenxuantra2k3@gmail.com>
2026-01-23 14:35:47 +0700

    code

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -560,7 +560,7 @@ const Dashboard = () => {
                         cx="50%"
                         cy="50%"
                         labelLine={false}
-                        label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+                        label={({ name, percent }) => `${name}: ${percent!==undefined && (percent * 100).toFixed(0)}%`}
                         outerRadius={100}
                         fill="#8884d8"
                         dataKey="value"
